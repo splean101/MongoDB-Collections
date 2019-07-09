dfe51a63f53e16d75c68ea048dc42bdf3ddab443
splean101 <101splean@gmail.com>
2019-07-09 15:26:07 +0300

    1

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'userdb';
 
-MongoClient.connect(url, { useNewUrlParser: true }, (err, client) =>{
+MongoClient.connect(url, (err, client) => {
     const db = client.db(dbName);
-    console.log(db);
-    client.close();
-    
-})
+    db.createCollection("users", (err, collection) => {
+        console.log(collection.collectionName);
+        db.renameCollection("users", "newUsers", (err, collection) => {
+            console.log(collection.collectionName);
+        });
+    });
+});
