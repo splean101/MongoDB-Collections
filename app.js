37af6b6469d938daeda4c5f2dd332cbb85da190d
splean101 <101splean@gmail.com>
2019-07-09 15:15:57 +0300

    db start

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'userdb';
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) =>{
+    const db = client.db(dbName);
+    console.log(db);
+    client.close();
+    
+})
