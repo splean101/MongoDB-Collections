b340173f4c00b2fc7826832f17bb2117f9917929
splean101 <101splean@gmail.com>
2019-07-10 10:23:15 +0300

    Finaly

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'userdb';
-
-MongoClient.connect(url, (err, client) => {
-    const db = client.db(dbName);
-    db.createCollection("users", (err, collection) => {
-        console.log(collection.collectionName);
-        db.renameCollection("users", "newUsers", (err, collection) => {
+MongoClient.connect(
+    'mongodb://localhost:27017',
+    { useNewUrlParser: true },
+    (err, client) => {
+        const db = client.db('userdb');
+        db.createCollection("collection1", (err, collection) => {
+            if (err) console.log('collection1 NOT DONE --> ');
             console.log(collection.collectionName);
         });
+        db.createCollection("collection2", (err, collection) => {
+            if (err) console.log('collection2 NOT DONE --> ' + err);
+            console.log(collection.collectionName);
+        });
+        db.renameCollection('collection1', 'collection2 ', (err, collection) => {
+            if (err) {
+                db.renameCollection('collection2', 'tmpCollection2 ', (err, collection) => {
+                    if (err) console.log('NOT DONE --> ' + err);
+                    console.log('collection2 renamed to tmpCollection2');
+                });
+            } else {
+                console.log('collection1 renamed to collection2');
+                console.log(collection.collectionName);
+            };
+        });
     });
-});
+/*
+;*/
