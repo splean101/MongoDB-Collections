2680203a038c9289faeb6937cdbf07a87cc8759c
splean101 <101splean@gmail.com>
2019-07-10 11:39:25 +0300

    fin

--- app.js
+++ app.js
@@ -15,8 +15,15 @@ MongoClient.connect(
         });
         db.renameCollection('collection1', 'collection2 ', (err, collection) => {
             if (err) {
+                console.log('collection1 --> collection2 NOT DONE --> ' + err);
                 db.renameCollection('collection2', 'tmpCollection2 ', (err, collection) => {
-                    if (err) console.log('NOT DONE --> ' + err);
+                    if (err) {
+                        console.log('tmpCollection2 NOT DONE --> ' + err);
+                    } else {
+                        db.renameCollection('collection1', 'collection2 ', (err, collection) => {
+                            console.log(`tmpCollection2 is DONE, collection name --> ${collection.collectionName}`);
+                        });
+                    };
                     console.log('collection2 renamed to tmpCollection2');
                 });
             } else {
@@ -25,5 +32,3 @@ MongoClient.connect(
             };
         });
     });
-/*
-;*/
